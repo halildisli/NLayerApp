08c704263ae795835d37370db4292577e4c0b7f6
Halil Ibrahim Disli <halil.disli@outlook.com>
2023-06-24 18:53:18 +0300

    Repository katmanı içerisinde Entity Configurations yapıldı. Db Context sınıfı oluşturuldu.

---
+++ NLayerApp/NLayer.Repository/Contexts/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Contexts
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Assembly demek aynı proje içerisinde yer almak demektir.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //Assembly deki tüm interfaceleri bulup uygualar.
+
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration()); // Bu kullanımda ise hangi configuration dosyalarını uygulayacağını tek tek göstermek gerekiyor.
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
