5d90f982f2b76b83f1054eba87f5dc6c6bb6c2aa
Halil Ibrahim Disli <halil.disli@outlook.com>
2023-07-04 16:16:33 +0300

    .net 7 ile Service katmanından doğrudan Dto nesnesi dönmek için refactoring işlemi yapıldı.

--- NLayerApp/NLayer.API/Modules/RepoServiceModule.cs
+++ NLayerApp/NLayer.API/Modules/RepoServiceModule.cs
@@ -21,6 +21,9 @@ namespace NLayer.API.Modules
             builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
             builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
 
+            builder.RegisterGeneric(typeof(ServiceWithDto<,>)).As(typeof(IServiceWithDto<,>)).InstancePerLifetimeScope();
+
+
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
 
             var apiAssembly = Assembly.GetExecutingAssembly();
